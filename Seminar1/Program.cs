e0c8330272636c6d707f4d4bd7f79fef958a89d3
Nikita <sorokovsky.ni@yandex.ru>
2022-11-22 21:09:55 +0300

    Done hometask1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Task 1
+Console.Clear() ;
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());// ввод чисел 
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32 (Console.ReadLine()); //ввод чисел
+if (a < b)
+ Console.Write("Yes,b = max");
+ else
+  Console.Write("NO");
+  //То же самое можно проделать с последним,где 3>9,(a>b)
+
